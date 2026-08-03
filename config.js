60b02ae298f72a2a74a5ea6fe57aa381f265d56c
faux-french-server <fradumbrel@gmail.com>
2026-08-03 21:14:13 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,1 @@
+const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQMjMGbQWhZN8bcQe_omRzqloetJEdMGLDylpPu-qVEL1kO0b-V3D44D7UYRBur_5qKivpko3ynpkkg/pub?gid=0&single=true&output=csv';
